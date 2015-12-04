1df458670cf37fec6aef761a89e63c6225f0bbb8
nidefawl <nidefawl@craftland.org>
2015-12-04 21:24:59 +0100

    convert config

---
+++ convertcfg.py
@@ -0,0 +1,7 @@
+
+outdir=r"C:\Users\Michael\workspace_projects\Game\res\textures\blocks\\"
+pathImageMagickDir=r"C:\Program Files\ImageMagick-6.9.2-Q16\\"
+pathImageMagick=pathImageMagickDir+"convert.exe"
+pathImageMagick2=pathImageMagickDir+"montage.exe"
+pathInkscape="D:\\Inkscape-0.91-1-win64\\inkscape\\inkscape.exe"
+onlyExportFileWithName="milkspice"
